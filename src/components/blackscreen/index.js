2e4508d389dc4993b596a95ad3b2aaac5834125b
Daniil Potapov <lancelap@mail.ru>
2021-07-26 19:52:27 +0300

    add ua

--- src/components/blackscreen/index.js
+++ src/components/blackscreen/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { Content, Text, Turn } from './styled'
 
 export default function Blackscreen() {
-    return (
-        <Content>
-            <Text
-                dangerouslySetInnerHTML={{
-                    __html: `увеличьте окно браузера, чтобы начать игру`,
-                }}
-            ></Text>
-            <Turn />
-        </Content>
-    )
+    if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            navigator.userAgent
+        )
+    ) {
+        return null
+    } else {
+        return (
+            <Content>
+                <Text
+                    dangerouslySetInnerHTML={{
+                        __html: `увеличьте окно браузера, чтобы начать игру`,
+                    }}
+                ></Text>
+                <Turn />
+            </Content>
+        )
+    }
 }
