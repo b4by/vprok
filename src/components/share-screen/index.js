073f7604a7eed1d8e504fca447c9b1add87d7e82
Leonid <comrur@gmail.com>
2021-07-29 15:54:14 +0300

    added share and meta images, added meta tags to index.html and share.html

--- src/components/share-screen/index.js
+++ src/components/share-screen/index.js
@@ -1,7 +1,12 @@
+import EndLevel from 'components/end-level/index'
 import {} from './styled'
 
 const ShareScreen = () => {
-    return <></>
+    return (
+        <EndLevel>
+            <div></div>
+        </EndLevel>
+    )
 }
 
 export default ShareScreen
