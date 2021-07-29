393eae2ffb5e2f909a46d23f30ccb369191141b2
Daniil Potapov <lancelap@mail.ru>
2021-07-29 21:05:35 +0300

    fix

--- src/components/start-game/index.js
+++ src/components/start-game/index.js
@@ -13,7 +13,7 @@ export const Wrapper = styled.div`
     right: 0;
     display: flex;
     width: 100%;
-    height: 100%;
+    height: 100vh;
     background: rgba(0, 0, 0, 0.7);
     display: flex;
     justify-content: center;
